eca9b6f4eefa9772ed68cc1f701c1a6f40296809
Eric García Castro <ericgar2003@gmail.com>
2024-09-30 18:41:34 +0200

    Fase 1 implementada y testeada

--- FASE1/src/Ubicacio.java
+++ FASE1/src/Ubicacio.java
@@ -1,63 +1,72 @@
-package src;
-
 public class Ubicacio {
 
-    private static final int PRECISION = 10000;
+    // Constants
+    private static final double TOLERANCIA = 0.0001;
     private static final double RADI_TERRA = 6378.137;
     private static final double PI_TO_RADIANTS = Math.PI / 180;
+
+    // Atributs
     private double latitud;
     private double longitud;
 
+    // Constructor de la ubicació
     public Ubicacio(double latitud, double longitud) {
         this.latitud = latitud;
         this.longitud = longitud;
     }
 
+    // Retorna la latitud de la ubicació
     public double getLatitud() {
         return latitud;
     }
 
+    // Retorna la longitud de la ubicació
     public double getLongitud() {
         return longitud;
     }
 
+    // Modifica la latitud de la ubicació
     public void setLatitud(double latitud) {
         this.latitud = latitud;
     }
 
+    // Modifica la longitud de la ubicació
     public void setLongitud(double longitud) {
         this.longitud = longitud;
     }
 
+    // Retorna la informació de la ubicació amb un format concret de text
     public String toString() {
         return "Ubicacio [latitud=" + getLatitud() + ", longitud=" + getLongitud() + "]";
     }
 
+    // Copia els valors de la ubicació actual a una nova ubicació que té assignada una posicion de memoria diferent
     public Ubicacio copiaUbicacio() {
         return new Ubicacio(this.getLatitud(), this.getLongitud());
     }
 
-    private double precisioXifres(double valor) {
-        return Math.round(valor * PRECISION) / (double) PRECISION;
+    // Compara si dues ubicacions són iguals
+    private boolean equalsUbicacio(Ubicacio ubicacio) {
+        return Math.abs(this.getLatitud() - ubicacio.getLatitud()) < TOLERANCIA // Si la diferencia es menor que la tolerancia llavors son iguals
+                && Math.abs(this.getLongitud() - ubicacio.getLongitud()) < TOLERANCIA;
     }
 
-    public boolean equalsUbicacio(Ubicacio ubicacio) {
-        return precisioXifres(this.getLatitud()) == precisioXifres(ubicacio.getLatitud())
-                && precisioXifres(this.getLongitud()) == precisioXifres(ubicacio.getLongitud());
-    }
-
+    // Converteix graus a radians
     private double toRadiants(double graus) {
         return graus * PI_TO_RADIANTS;
     }
 
+    // Calcula el valor a de la fórmula de Haversine
     private double calculateA(double dlat, double dlon, double lat1, double lat2) {
         return Math.pow(Math.sin(dlat / 2), 2) + Math.cos(lat1) * Math.cos(lat2) * Math.pow(Math.sin(dlon / 2), 2);
     }
 
+    // Calcula la distància entre dos punts amb la fórmula de Haversine
     private double calculateDistance(double a) {
         return RADI_TERRA * 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
     }
 
+    // Calcula la distància entre dues ubicacions
     public double distancia(Ubicacio ubicacio) {
 
         double lat1, lon1, lat2, lon2, dlat, dlon, a, distancia = 0;
--- FASE1/src/UsaUbicacio.java
+++ FASE1/src/UsaUbicacio.java
@@ -1,7 +1,77 @@
-package src;
-
 public class UsaUbicacio {
+
     public static void main(String[] args) {
-       
+        // Proves per a constructor i getters
+        Ubicacio u1 = new Ubicacio(41.3879, 2.16992); // Barcelona
+        Ubicacio u2 = new Ubicacio(48.8566, 2.3522); // París
+        
+        // Test dels getters
+        System.out.println("Prova getters:");
+        if (u1.getLatitud() != 41.3879 || u1.getLongitud() != 2.16992 || u2.getLatitud() != 48.8566 || u2.getLongitud() != 2.3522) {
+            System.out.println("Incorrecte!\n");
+        } else {
+            System.out.println("Correcte!\n");
+        }   
+
+        // Proves dels setters
+        System.out.println("Prova setters:");
+        u1.setLatitud(40.4168);
+        u1.setLongitud(-3.7038);
+
+        if (u1.getLatitud() != 40.4168 || u1.getLongitud() != -3.7038) {
+            System.out.println("Incorrecte!\n");
+            
+        } else {
+            System.out.println("Correcte!\n");
+        }
+ 
+        // Prova del mètode toString
+        System.out.println("Prova toString:");
+        String expectedString = "Ubicacio [latitud=40.4168, longitud=-3.7038]"; // Variable de tipus String amb el resultat esperat
+        if (u1.toString().equals(expectedString)) {
+            System.out.println("Correcte!\n");
+        } else {
+            System.out.println("Incorrecte!\n");
+        }
+        
+
+        // Prova del mètode copiaUbicacio
+        System.out.println("Prova copiaUbicacio:");
+        Ubicacio copia = u1.copiaUbicacio();
+        if (copia.getLatitud() != u1.getLatitud() || copia.getLongitud() != u1.getLongitud()) {
+            System.out.println("Incorrecte!\n");
+        } else {
+            System.out.println("Correcte!\n");
+        }
+  
+        
+
+        // Prova del mètode equalsUbicacio (a través de distancia, ja que és privat)
+        System.out.println("Prova equalsUbicacio a través de distancia:");
+        Ubicacio u3 = new Ubicacio(45.323425, -4.2342446); // Ubicació amb 6 i 7 decimals
+        Ubicacio u4 = new Ubicacio(45.323425323, -4.234244643); // Ubicació amb 9 i 9 decimals pero diferent respecte u3
+        Ubicacio u5 = new Ubicacio(45.323425342, -4.234244635); // Ubicació amb 9 i 9 decimals pero diferent respecte u4
+        Ubicacio u6 = new Ubicacio(45.1234, -4.1234); // Ubicació amb 4 i 4 decimals
+        Ubicacio u7 = new Ubicacio(45.1234, -4.1234); // Ubicació amb 4 i 4 decimals igual que u7
+        Ubicacio u8 = new Ubicacio(45.1, -4.1); // Ubicació amb 1 i 1 decimals
+        Ubicacio u9 = new Ubicacio(45.1, -4.1); // Ubicació amb 1 i 1 decimals igual que u8
+
+        if (u3.distancia(u4) == 0  && u4.distancia(u5) == 0 && u6.distancia(u7) == 0 && u8.distancia(u9) == 0) {
+            System.out.println("Correcte!\n");
+        } else {
+            System.out.println("Incorrecte!\n");
+        }
+        
+
+        // Prova de la distància entre dues ubicacions diferents
+        System.out.println("Prova distancia entre dues ubicacions diferents(París-Barcelona):");
+        Ubicacio u10 = new Ubicacio(41.3879, 2.16992); // Barcelona
+        double distanciaBCNParis = u2.distancia(u10); // Distància entre París i Barcelona
+        System.out.printf("Distància entre París i Barcelona: %.2f km%n", distanciaBCNParis);
+        if (Math.abs(distanciaBCNParis - 830.0) < 10) { // Aproximadament 830 km(830,61 km segons internet) amb un marge d'error de 10 km
+            System.out.println("Correcte!\n");
+        } else {
+            System.out.println("Incorrecte!\n");
+        }
     }
 }
