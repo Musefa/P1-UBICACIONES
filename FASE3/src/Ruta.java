37ff7efe9edcf40afc69108c24ef23bbef204f64
Eric García Castro <ericgar2003@gmail.com>
2024-10-16 14:27:29 +0200

    Fase 3 acabada

--- FASE3/src/Ruta.java
+++ FASE3/src/Ruta.java
@@ -3,6 +3,11 @@ public class Ruta {
     private boolean acabaEnOrigen;
     private int numUbicacions;
 
+    /**
+     * Constructor de la classe Ruta.
+     * 
+     * @param mida Mida inicial de l'array d'ubicacions.
+     */
     public Ruta(int mida) {
         if (mida > 0) {
             this.ubicacions = new Ubicacio[mida];
@@ -13,6 +18,12 @@ public class Ruta {
         this.acabaEnOrigen = false;
     }
 
+    /**
+     * Constructor de la classe Ruta.
+     * 
+     * @param mida          Mida inicial de l'array d'ubicacions.
+     * @param acabaEnOrigen Indica si la ruta acaba en l'origen.
+     */
     public Ruta(int mida, boolean acabaEnOrigen) {
         if (mida > 0) {
             this.ubicacions = new Ubicacio[mida];
@@ -23,33 +34,57 @@ public class Ruta {
         this.acabaEnOrigen = acabaEnOrigen;
     }
 
-    // Getters i setters
+    /**
+     * Obté l'array d'ubicacions.
+     * 
+     * @return Array d'ubicacions.
+     */
     public Ubicacio[] getUbicacions() {
         return ubicacions;
     }
 
+    /**
+     * Estableix l'array d'ubicacions.
+     * 
+     * @param ubicacions Array d'ubicacions.
+     */
     public void setUbicacions(Ubicacio[] ubicacions) {
         this.ubicacions = ubicacions;
     }
 
+    /**
+     * Indica si la ruta acaba en l'origen.
+     * 
+     * @return true si la ruta acaba en l'origen, false en cas contrari.
+     */
     public boolean getAcabaEnOrigen() {
         return acabaEnOrigen;
     }
 
+    /**
+     * Estableix si la ruta acaba en l'origen.
+     * 
+     * @param acabaEnOrigen true si la ruta acaba en l'origen, false en cas
+     *                      contrari.
+     */
     public void setAcabaEnOrigen(boolean acabaEnOrigen) {
         this.acabaEnOrigen = acabaEnOrigen;
     }
 
+    /**
+     * Obté el nombre d'ubicacions.
+     * 
+     * @return Nombre d'ubicacions.
+     */
     public int getNumUbicacions() {
         return numUbicacions;
     }
 
-    /*
-     * public void setNumUbicacions(int numUbicacions) {
-     * this.numUbicacions = numUbicacions;
-     * }
+    /**
+     * Afegeix una nova ubicació a la ruta.
+     * 
+     * @param ubicacio Ubicació a afegir.
      */
-
     public void afegirUbicacio(Ubicacio ubicacio) {
         if (numUbicacions == 0) {
             ubicacions[numUbicacions++] = ubicacio;
@@ -59,6 +94,38 @@ public class Ruta {
         }
     }
 
+    /**
+     * Consulta una ubicació en una posició específica.
+     * 
+     * @param posicio Posició de la ubicació a consultar.
+     * @return Ubicació en la posició especificada, o null si la posició és
+     *         invàlida.
+     */
+    public Ubicacio consultarUbicacio(int posicio) {
+        Ubicacio ubicacio = null;
+        if (posicio >= 0 && posicio < numUbicacions) {
+            ubicacio = ubicacions[posicio];
+        }
+        return ubicacio;
+    }
+
+    /**
+     * Modifica una ubicació en una posició específica.
+     * 
+     * @param posicio  Posició de la ubicació a modificar.
+     * @param ubicacio Nova ubicació.
+     */
+    public void modificarUbicacio(int posicio, Ubicacio ubicacio) {
+        if (posicio >= 0 && posicio < numUbicacions) {
+            ubicacions[posicio] = ubicacio;
+        }
+    }
+
+    /**
+     * Calcula la distància total de la ruta.
+     * 
+     * @return Distància total de la ruta.
+     */
     public double calcularDistanciaRuta() {
         double distancia = 0.0;
         for (int i = 0; i < numUbicacions - 1; i++) {
@@ -70,6 +137,11 @@ public class Ruta {
         return distancia;
     }
 
+    /**
+     * Obté la ubicació més al nord de la ruta.
+     * 
+     * @return Ubicació més al nord.
+     */
     public Ubicacio ubicacioMesAlNord() {
         Ubicacio mesAlNord = ubicacions[0];
         for (int i = 1; i < numUbicacions; i++) {
@@ -80,11 +152,16 @@ public class Ruta {
         return mesAlNord;
     }
 
+    /**
+     * Torna una representació en cadena de la ruta.
+     * 
+     * @return Representació en cadena de la ruta.
+     */
     public String toString() {
-        String aux = "Ruta => Nombre de ubicacions: " + numUbicacions;
+        String aux = "Ruta => Nombre d'ubicacions: " + numUbicacions;
 
         if (getAcabaEnOrigen()) {
-            aux = aux + "\n\tLa ruta acaba en el origen:";
+            aux = aux + "\n\tLa ruta acaba en l'origen:";
         }
 
         for (int i = 0; i < numUbicacions; i++) {
--- FASE3/src/UsaRuta.java
+++ FASE3/src/UsaRuta.java
@@ -1,24 +1,46 @@
 public class UsaRuta {
     public static void main(String[] args) {
-        // Proves per a constructor i getters
-        System.out.println("Proves per a constructors:");
-
+        // Proves per a constructors, setters i getters
+        System.out.println("Proves per a constructors, setters i getters:");
         Ruta r1 = new Ruta(5);
         Ruta r2 = new Ruta(5);
 
-        if (r1.getNumUbicacions() == 0 && r2.getNumUbicacions() == 0) {
+        if (!r1.getAcabaEnOrigen() && !r2.getAcabaEnOrigen() && r1.getNumUbicacions() == 0
+                && r2.getNumUbicacions() == 0) {
             System.out.println("Correcte!\n");
         } else {
             System.out.println("Incorrecte!\n");
         }
 
+        Ruta r3 = new Ruta(5, true);
+        Ruta r4 = new Ruta(5, false);
+
+        if (r3.getAcabaEnOrigen() && !r4.getAcabaEnOrigen() && r3.getNumUbicacions() == 0
+                && r4.getNumUbicacions() == 0) {
+            System.out.println("Correcte!\n");
+        } else {
+            System.out.println("Incorrecte!\n");
+
+        }
+
+        r1.setAcabaEnOrigen(true);
+        r1.setAcabaEnOrigen(false);
+        r2.setAcabaEnOrigen(true);
+
+        if (!r1.getAcabaEnOrigen() && r2.getAcabaEnOrigen()) {
+            System.out.println("Correcte!\n");
+        } else {
+            System.out.println("Incorrecte!\n");
+
+        }
+
         // Proves del mètode afegirUbicacio
         System.out.println("Prova afegirUbicacio:");
-        Ubicacio u1 = new Ubicacio(41.3879, 2.16992); // Barcelona
-        Ubicacio u2 = new Ubicacio(48.8566, 2.3522); // París
-        Ubicacio u3 = new Ubicacio(40.7128, -74.0060); // Nova York
-        Ubicacio u4 = new Ubicacio(34.0522, -118.2437); // Los Angeles
-        Ubicacio u5 = new Ubicacio(37.7749, -122.4194); // San Francisco
+        Ubicacio u1 = new Ubicacio(41.3879, 2.16992);
+        Ubicacio u2 = new Ubicacio(48.8566, 2.3522);
+        Ubicacio u3 = new Ubicacio(40.7128, -74.0060);
+        Ubicacio u4 = new Ubicacio(34.0522, -118.2437);
+        Ubicacio u5 = new Ubicacio(37.7749, -122.4194);
 
         r1.afegirUbicacio(u1);
         r1.afegirUbicacio(u2);
@@ -38,36 +60,31 @@ public class UsaRuta {
             System.out.println("Incorrecte!\n");
         }
 
-        // Proves del mètode setAcabaEnOrigen
-        System.out.println("Prova setAcabaEnOrigen:");
-        r1.setAcabaEnOrigen(true);
-        r1.setAcabaEnOrigen(false);
-        r2.setAcabaEnOrigen(true);
+        // Proves del mètode calcularDistanciaRuta
+        System.out.println("Prova calcularDistanciaRuta:");
+        System.out.println(r1.calcularDistanciaRuta());
+        System.out.println(r2.calcularDistanciaRuta());
 
-        if (!r1.getAcabaEnOrigen() && r2.getAcabaEnOrigen()) {
+        if (r1.calcularDistanciaRuta() == 11175.216543006276 && r2.calcularDistanciaRuta() == 20757.70138469177) {
             System.out.println("Correcte!\n");
         } else {
             System.out.println("Incorrecte!\n");
 
         }
 
-        // Proves del mètode calcularDistanciaRuta
-        System.out.println("Prova calcularDistanciaRuta:");
-        System.out.println(r1.calcularDistanciaRuta());
-        System.out.println(r2.calcularDistanciaRuta());
-
-        // Prove del mètode toString
+        // Proves del mètode toString
         System.out.println("Prova toString:");
-        System.out.println(r1.toString());
-        System.out.println(r2.toString());
+        System.out.println(r1);
+        System.out.println("\n");
+        System.out.println(r2);
+
         if (r1.toString().equals(
-                "Ruta => Nombre de ubicacions: 5\n\tUbicacio posicio 0: Ubicacio [latitud=41.3879, longitud=2.16992]\n\tUbicacio posicio 1: Ubicacio [latitud=48.8566, longitud=2.3522]\n\tUbicacio posicio 2: Ubicacio [latitud=40.7128, longitud=-74.006]\n\tUbicacio posicio 3: Ubicacio [latitud=34.0522, longitud=-118.2437]\n\tUbicacio posicio 4: Ubicacio [latitud=37.7749, longitud=-122.4194]")
+                "Ruta => Nombre d'ubicacions: 5\n\tUbicacio posicio 0: Ubicacio [latitud=41.3879, longitud=2.16992]\n\tUbicacio posicio 1: Ubicacio [latitud=48.8566, longitud=2.3522]\n\tUbicacio posicio 2: Ubicacio [latitud=40.7128, longitud=-74.006]\n\tUbicacio posicio 3: Ubicacio [latitud=34.0522, longitud=-118.2437]\n\tUbicacio posicio 4: Ubicacio [latitud=37.7749, longitud=-122.4194]")
                 && r2.toString().equals(
-                        "Ruta => Nombre de ubicacions: 5\n\tLa ruta acaba en el origen:\n\tUbicacio posicio 0: Ubicacio [latitud=41.3879, longitud=2.16992]\n\tUbicacio posicio 1: Ubicacio [latitud=48.8566, longitud=2.3522]\n\tUbicacio posicio 2: Ubicacio [latitud=40.7128, longitud=-74.006]\n\tUbicacio posicio 3: Ubicacio [latitud=34.0522, longitud=-118.2437]\n\tUbicacio posicio 4: Ubicacio [latitud=37.7749, longitud=-122.4194]")) {
+                        "Ruta => Nombre d'ubicacions: 5\n\tLa ruta acaba en l'origen:\n\tUbicacio posicio 0: Ubicacio [latitud=41.3879, longitud=2.16992]\n\tUbicacio posicio 1: Ubicacio [latitud=48.8566, longitud=2.3522]\n\tUbicacio posicio 2: Ubicacio [latitud=40.7128, longitud=-74.006]\n\tUbicacio posicio 3: Ubicacio [latitud=34.0522, longitud=-118.2437]\n\tUbicacio posicio 4: Ubicacio [latitud=37.7749, longitud=-122.4194]")) {
             System.out.println("Correcte!\n");
         } else {
             System.out.println("Incorrecte!\n");
-
         }
 
         // Proves del mètode ubicacioMesAlNord
@@ -79,7 +96,30 @@ public class UsaRuta {
             System.out.println("Correcte!\n");
         } else {
             System.out.println("Incorrecte!\n");
+        }
 
+        // Proves del mètode consultarUbicacio
+        System.out.println("Prova consultarUbicacio:");
+        String expectedString = "Ubicacio [latitud=41.3879, longitud=2.16992]";
+        System.out.println(r1.consultarUbicacio(0));
+
+        if (r1.consultarUbicacio(0).toString().equals(expectedString)) {
+            System.out.println("Correcte!\n");
+        } else {
+            System.out.println("Incorrecte!\n");
+        }
+
+        // Proves del mètode modificarUbicacio
+        System.out.println("Prova modificarUbicacio:");
+        Ubicacio u6 = new Ubicacio(51.5074, 0.1278);
+        r1.modificarUbicacio(0, u6);
+        System.out.println(r1.consultarUbicacio(0));
+        expectedString = "Ubicacio [latitud=51.5074, longitud=0.1278]";
+
+        if (r1.consultarUbicacio(0).toString().equals(expectedString)) {
+            System.out.println("Correcte!\n");
+        } else {
+            System.out.println("Incorrecte!\n");
         }
     }
 }
